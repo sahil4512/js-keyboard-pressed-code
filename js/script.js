22e550ed23fc69c2dcac4053ead6bf568b22e48f
sahilkumardev <KUMAR.SAHIL4512@GMAIL.COM>
2022-04-19 01:48:49 -0400

    updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const text_key = document.getElementById("keycode");
 const dis_play_pressed = document.getElementById("text");
+const key_board_key = document.getElementById("keyboard-key");
 
 window.addEventListener("keydown", function (e) {
-  const key = e.keyCode;
+  const key_board = e.keyCode;
   dis_play_pressed.style.display = "block";
   text_key.innerHTML = "";
-  text_key.innerHTML = key;
+  text_key.innerHTML = key_board;
 });
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Keyboard Key Pressed Code
+
+### Screenshot
+
+![Solutions ScreenShot](./css/working.gif))
+
+### Links
+
+- Live Site URL: [Live Demo](https://superlative-treacle-26e32b.netlify.app/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- javascript
+
+## Author
+
+- Codepen- [sahil4512](https://codepen.io/sahil4512)
+- Twitter - [@sahil_4512](https://www.twitter.com/sahil_4512)
